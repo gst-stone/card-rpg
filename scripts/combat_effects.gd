31d9af5149555c1a83bfaf2a105053651d4ca8e6
gst-stone <gaoshutong@gmail.com>
2026-08-23 20:01:21 +0800

    fix: include relic defense and lifesteal in combat calculation

--- scripts/combat_effects.gd
+++ scripts/combat_effects.gd
@@ -3,6 +3,10 @@ extends RefCounted
 
 static func apply_card(card: Dictionary, enemy: Dictionary, player: RunData, relics: Array, energy: int) -> Dictionary:
 	var damage := int(card.get("damage", 0)) + RelicCatalog.damage_bonus(relics)
-	var block := int(card.get("block", 0))
+	var block := int(card.get("block", 0)) + RelicCatalog.block_bonus(relics)
 	var heal := int(card.get("heal", 0))
+	if damage > 0: heal += RelicCatalog.lifesteal(relics)
 	return {"damage": damage, "block": block, "heal": heal, "energy": energy - int(card.get("cost", 0))}
+
+static func enemy_phase_damage(base_damage: int, status: StatusEffects) -> int:
+	return status.reduce_damage(base_damage)
