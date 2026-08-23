78838cdf6299cdbcc321e0ed877d116d2c8e8612
gst-stone <gaoshutong@gmail.com>
2026-08-23 21:08:23 +0800

    feat: classify cards into attack and skill types

--- scripts/card_catalog.gd
+++ scripts/card_catalog.gd
@@ -2,20 +2,20 @@ class_name CardCatalog
 extends RefCounted
 
 const CARDS := {
-	"Strike": {"cost": 1, "damage": 15, "heal": 0, "block": 0, "description": "Deal 15 damage."},
-	"Strike+": {"cost": 1, "damage": 20, "heal": 0, "block": 0, "description": "Deal 20 damage."},
-	"Fireball": {"cost": 2, "damage": 25, "heal": 0, "block": 0, "description": "Deal 25 damage."},
-	"Fireball+": {"cost": 2, "damage": 34, "heal": 0, "block": 0, "description": "Deal 34 damage."},
-	"Guard": {"cost": 1, "damage": 0, "heal": 10, "block": 8, "description": "Gain 8 block and heal 10 HP."},
-	"Guard+": {"cost": 1, "damage": 0, "heal": 14, "block": 12, "description": "Gain 12 block and heal 14 HP."},
-	"Heavy Blow": {"cost": 3, "damage": 40, "heal": 0, "block": 0, "description": "Deal 40 damage."},
-	"Heavy Blow+": {"cost": 2, "damage": 45, "heal": 0, "block": 0, "description": "Deal 45 damage."},
-	"Slash": {"cost": 1, "damage": 22, "heal": 0, "block": 0, "description": "Deal 22 damage."},
-	"Slash+": {"cost": 1, "damage": 29, "heal": 0, "block": 0, "description": "Deal 29 damage."},
-	"Ice Lance": {"cost": 2, "damage": 30, "heal": 0, "block": 0, "description": "Deal 30 damage."},
-	"Ice Lance+": {"cost": 2, "damage": 40, "heal": 0, "block": 0, "description": "Deal 40 damage."},
-	"Blood Pact": {"cost": 1, "damage": 18, "heal": 8, "block": 0, "description": "Deal 18 damage and heal 8 HP."},
-	"Blood Pact+": {"cost": 1, "damage": 24, "heal": 12, "block": 0, "description": "Deal 24 damage and heal 12 HP."}
+	"Strike": {"cost": 1, "damage": 15, "heal": 0, "block": 0, "type": "attack", "description": "Deal 15 damage."},
+	"Strike+": {"cost": 1, "damage": 20, "heal": 0, "block": 0, "type": "attack", "description": "Deal 20 damage."},
+	"Fireball": {"cost": 2, "damage": 25, "heal": 0, "block": 0, "type": "attack", "description": "Deal 25 damage."},
+	"Fireball+": {"cost": 2, "damage": 34, "heal": 0, "block": 0, "type": "attack", "description": "Deal 34 damage."},
+	"Guard": {"cost": 1, "damage": 0, "heal": 10, "block": 8, "type": "skill", "description": "Gain 8 block and heal 10 HP."},
+	"Guard+": {"cost": 1, "damage": 0, "heal": 14, "block": 12, "type": "skill", "description": "Gain 12 block and heal 14 HP."},
+	"Heavy Blow": {"cost": 3, "damage": 40, "heal": 0, "block": 0, "type": "attack", "description": "Deal 40 damage."},
+	"Heavy Blow+": {"cost": 2, "damage": 45, "heal": 0, "block": 0, "type": "attack", "description": "Deal 45 damage."},
+	"Slash": {"cost": 1, "damage": 22, "heal": 0, "block": 0, "type": "attack", "description": "Deal 22 damage."},
+	"Slash+": {"cost": 1, "damage": 29, "heal": 0, "block": 0, "type": "attack", "description": "Deal 29 damage."},
+	"Ice Lance": {"cost": 2, "damage": 30, "heal": 0, "block": 0, "type": "attack", "description": "Deal 30 damage."},
+	"Ice Lance+": {"cost": 2, "damage": 40, "heal": 0, "block": 0, "type": "attack", "description": "Deal 40 damage."},
+	"Blood Pact": {"cost": 1, "damage": 18, "heal": 8, "block": 0, "type": "skill", "description": "Deal 18 damage and heal 8 HP."},
+	"Blood Pact+": {"cost": 1, "damage": 24, "heal": 12, "block": 0, "type": "skill", "description": "Deal 24 damage and heal 12 HP."}
 }
 
 const upgraded_cards := {
@@ -25,6 +25,9 @@ const upgraded_cards := {
 
 static func get_card(name: String) -> Dictionary:
 	return CARDS.get(name, {})
+
+static func card_type(name: String) -> String:
+	return str(get_card(name).get("type", "skill"))
 
 static func all_names() -> Array:
 	return CARDS.keys()
