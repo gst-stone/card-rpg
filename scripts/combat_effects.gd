714a50eed4b31bfb7501a0bb047ffd38dd1a297f
gst-stone <gaoshutong@gmail.com>
2026-08-27 20:13:35 +0800

    feat: propagate card status effects

--- scripts/combat_effects.gd
+++ scripts/combat_effects.gd
@@ -6,7 +6,16 @@ static func apply_card(card: Dictionary, enemy: Dictionary, player: RunData, rel
 	var block := int(card.get("block", 0)) + RelicCatalog.block_bonus(relics)
 	var heal := int(card.get("heal", 0))
 	if damage > 0: heal += RelicCatalog.lifesteal(relics)
-	return {"damage": damage, "block": block, "heal": heal, "draw": int(card.get("draw", 0)), "energy": energy - int(card.get("cost", 0))}
+	return {
+		"damage": damage,
+		"block": block,
+		"heal": heal,
+		"draw": int(card.get("draw", 0)),
+		"vulnerable": int(card.get("vulnerable", 0)),
+		"weak": int(card.get("weak", 0)),
+		"poison": int(card.get("poison", 0)),
+		"energy": energy - int(card.get("cost", 0))
+	}
 
 static func enemy_phase_damage(base_damage: int, status: StatusEffects) -> int:
 	return status.reduce_damage(base_damage)
